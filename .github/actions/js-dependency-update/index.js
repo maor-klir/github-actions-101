7d65c4b091127777f8311c36119f501c0ae1ae72
maor-klir <maor.klir@gmail.com>
2025-05-20 17:16:51 +0200

    feat (17-custom-actions): add logic to create PR

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,6 +2,7 @@
 // Import the package on JavaScript
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
@@ -9,13 +10,15 @@ const validateDirectoryName = ({ dirName }) =>
   /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-    const baseBranch = core.getInput('base-branch');
-  const targetBranch = core.getInput('target-branch');
-  const ghToken = core.getInput('gh-token');
-  const workingDir = core.getInput('working-directory');
+  const baseBranch = core.getInput('base-branch', { required: true });
+  const targetBranch = core.getInput('target-branch', { required: true });
+  const ghToken = core.getInput('gh-token', { required: true });
+  const workingDir = core.getInput('working-directory', { required: true });
   const debug = core.getBooleanInput('debug');
 
-
+  const commonExecOpts = {
+    cwd: workingDir,
+  };
   core.setSecret(ghToken);
 
   if (!validateBranchName({ branchName: baseBranch })) {
@@ -44,7 +47,7 @@ async function run() {
   core.info(`[js-dependency-update] : working directory is ${workingDir}`);
 
   await exec.exec('npm update', [], {
-    cwd: workingDir,
+    ...commonExecOpts
   });
 
 
@@ -52,18 +55,48 @@ async function run() {
     'git status -s package*.json',
     [],
     {
-      cwd: workingDir,
+      ...commonExecOpts
     }
   );
 
-    if (gitStatus.stdout.length > 0) {
-    core.info('[js-dependency-update] : There are updates available!');
-  } else {
-    core.info('[js-dependency-update] : No updates at this point in time.');
-  }
+  if (gitStatus.stdout.length > 0) {
+  core.info('[js-dependency-update] : There are updates available!');
+  await exec.exec(`git config --global user.name "gh-autumation"`)
+  await exec.exec(`git config --global user-email "gh-automation.email.com"`)
+  await exec.exec(`git checkout -b ${targetBranch}`, [], {
+    ...commonExecOpts,
+  });
+    await exec.exec(`git add package.json package-lock.json`, [], {
+    ...commonExecOpts,
+  });
+    await exec.exec(`git commit -m "chore: update dependencies`, [], {
+    ...commonExecOpts,
+  });
+      await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+    ...commonExecOpts,
+  });
+  const octokit = github.getOctokit(ghToken);
 
-  core.info('I am a custom JS action')
+  try {
+  await octokit.rest.pulls.create({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    title: 'Update NPM dependencies',
+    body: 'This pull request updates NPM packages',
+    base: baseBranch,
+    head: targetBranch
+  });
+} catch (e) {
+  core.error('[js-dependency-update]: Something went wrong while creating the PR. Check logs below')
+  core.setFailed(e.message);
+  core.error(e);
 }
+} else {
+  core.info('[js-dependency-update] : No updates at this point in time.');
+}
+}
+run ()
+
 /*
 [DONE] 1. Parse inputs:
   1.1 base-branch from which to check for updates
@@ -71,11 +104,9 @@ async function run() {
   1.3 GitHub token for authentication purposes (to create PRs)
   1.4 Working directory for which to check for dependencies
 [DONE] 2. Execute the npm update command within the working directory
-[DONE ]3. Check whether there are modified package*.json files
-4. If there are modified files:
+[DONE] 3. Check whether there are modified package*.json files
+[DONE] 4. If there are modified files:
   4.1 Add and commit files to the target-branch
   4.2 Create a PR to the base-branch using the octokit API
-5. Otherwise, conclude the custom action
+[DONE] 5. Otherwise, conclude the custom action
 */
-
-run ()
