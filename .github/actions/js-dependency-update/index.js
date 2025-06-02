af91746afba6af3d81e51dea4d705c3b5ad702be
Maor Klir <maor.klir@gmail.com>
2025-06-02 13:22:40 +0200

    fix (17-custom-actions): fix typos

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -61,7 +61,7 @@ async function run() {
 
   if (gitStatus.stdout.length > 0) {
   core.info('[js-dependency-update] : There are updates available!');
-  await exec.exec(`git config --global user.name "gh-automation"`);Add commentMore actions
+  await exec.exec(`git config --global user.name "gh-automation"`);
   await exec.exec(`git config --global user.email "gh-automation@email.com"`);
   await exec.exec(`git checkout -b ${targetBranch}`, [], {
     ...commonExecOpts,
